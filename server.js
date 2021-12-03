ccf305660c60e2d180bab0444dff251121189999
Meiwang1248 <meiwang1248@hotmail.com>
2021-12-02 22:53:18 -0800

    server rough draft

---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const mongoose = require('mongoose');
+
+// const mongoAtlasUri = process.env.MONGODB_URI;
+
+const mongoAtlasUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`
+const options = {useNewUrlParser: true, useUnifiedTopology: true};
+
+try {
+    mongoose.connect(
+        mongoAtlasUri,
+        options,
+        () => console.log('connected'));
+} catch (e) {
+    console.log('could not connect', e);
+}
+
+const session = require('express-session')
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+const cors = require('cors');
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
+// `${process.env.ALLOW_ORIGIN}`
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+require("./controllers/user-controller")(app)
+require("./controllers/review-controller")(app)
+require("./controllers/favorite-controller")(app)
+
+app.listen(process.env.PORT || 3001)
+console.log("node server is running!")
